2d7a9cccbfff447cc0a6190e747e27c6cebfccea
Lifanwu <v-lifanwu@microsoft.com>
2023-04-10 07:34:52 +0000

    change the licences for scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 #!/usr/bin/env bash
 
 perr() {
