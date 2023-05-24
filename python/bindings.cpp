eed64e960c0397d3c1ad3d80a864864c17d3d552
Lifanwu <v-lifanwu@microsoft.com>
2023-05-24 09:10:23 +0000

    add the model definition

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -17,20 +17,25 @@ PYBIND11_MODULE(ark, m)
     m.doc() = "pybind11 ark plugin"; // optional module docstring
     m.def("init", &ark::init, "A function that initializes the ark");
 
-    m.def("srand", &ark::srand, py::arg("seed") = -1, "Sets the seed for the random number generator");  
-    m.def("rand", &ark::rand, "Generates a random integer"); 
-    m.attr("NO_DIM") = py::int_(static_cast<int>(ark::NO_DIM));  
-    m.attr("DIMS_LEN") = py::int_(static_cast<int>(ark::DIMS_LEN)); 
+    m.def("srand", &ark::srand, py::arg("seed") = -1,
+          "Sets the seed for the random number generator");
+    m.def("rand", &ark::rand, "Generates a random integer");
+    m.attr("NO_DIM") = py::int_(static_cast<int>(ark::NO_DIM));
+    m.attr("DIMS_LEN") = py::int_(static_cast<int>(ark::DIMS_LEN));
 
     py::class_<ark::Dims>(m, "Dims")
         .def(py::init([](ark::DimType d0, ark::DimType d1, ark::DimType d2,
                          ark::DimType d3) {
                  return std::make_unique<ark::Dims>(d0, d1, d2, d3);
              }),
-             py::arg_v("d0", static_cast<int>(ark::NO_DIM), "default value: NO_DIM"),
-             py::arg_v("d1", static_cast<int>(ark::NO_DIM), "default value: NO_DIM"),
-             py::arg_v("d2", static_cast<int>(ark::NO_DIM), "default value: NO_DIM"),
-             py::arg_v("d3", static_cast<int>(ark::NO_DIM), "default value: NO_DIM"))
+             py::arg_v("d0", static_cast<int>(ark::NO_DIM),
+                       "default value: NO_DIM"),
+             py::arg_v("d1", static_cast<int>(ark::NO_DIM),
+                       "default value: NO_DIM"),
+             py::arg_v("d2", static_cast<int>(ark::NO_DIM),
+                       "default value: NO_DIM"),
+             py::arg_v("d3", static_cast<int>(ark::NO_DIM),
+                       "default value: NO_DIM"))
         .def(py::init<const ark::Dims &>())
         .def(py::init<const std::vector<ark::DimType> &>())
         .def("size", &ark::Dims::size)
@@ -44,4 +49,148 @@ PYBIND11_MODULE(ark, m)
             os << d;
             return os.str();
         });
+
+    // Register TensorType
+    py::enum_<ark::TensorType>(m, "TensorType")
+        .value("FP16", ark::TensorType::FP16)
+        .value("FP32", ark::TensorType::FP32)
+        .value("INT32", ark::TensorType::INT32)
+        .export_values();
+
+    // Register TensorBuf
+    py::class_<ark::TensorBuf>(m, "TensorBuf")
+        .def(py::init<const ark::DimType &, int>(), py::arg("bytes") = 0,
+             py::arg("id") = -1)
+        .def_readwrite("bytes", &ark::TensorBuf::bytes)
+        .def_readwrite("id", &ark::TensorBuf::id)
+        .def_readwrite("immutable", &ark::TensorBuf::immutable);
+
+    // Register Tensor
+    py::class_<ark::Tensor>(m, "Tensor")
+        .def(py::init<const ark::Dims &, ark::TensorType, ark::TensorBuf *,
+                      const ark::Dims &, const ark::Dims &, const ark::Dims &,
+                      bool, bool, int, const std::string &>(),
+             py::arg("shape"), py::arg("type"), py::arg("buf"),
+             py::arg("ldims"), py::arg("offs"), py::arg("pads"),
+             py::arg("exported"), py::arg("imported"), py::arg("id"),
+             py::arg("name"))
+        .def("offset", &ark::Tensor::offset, py::arg("i0") = 0,
+             py::arg("i1") = 0, py::arg("i2") = 0, py::arg("i3") = 0)
+        .def("size", &ark::Tensor::size)
+        .def("ndims", &ark::Tensor::ndims)
+        .def("padded_shape", &ark::Tensor::padded_shape)
+        .def("type_bytes", &ark::Tensor::type_bytes)
+        .def("shape_bytes", &ark::Tensor::shape_bytes)
+        .def("ldims_bytes", &ark::Tensor::ldims_bytes)
+        .def("offset_bytes", &ark::Tensor::offset_bytes, py::arg("i0") = 0,
+             py::arg("i1") = 0, py::arg("i2") = 0, py::arg("i3") = 0);
+
+    py::class_<ark::Model>(m, "Model")
+        .def(py::init<>())
+        .def("tensor", &ark::Model::tensor,
+             py::return_value_policy::reference_internal, py::arg("shape"),
+             py::arg("dtype"), py::arg("buf") = nullptr,
+             py::arg("ldims") = ark::Dims(), py::arg("offs") = ark::Dims(),
+             py::arg("pads") = ark::Dims(),
+             py::arg("deps") = std::vector<ark::Tensor *>(),
+             py::arg("exported") = false, py::arg("imported") = false,
+             py::arg("name") = "tensor")
+        .def("reshape",
+             (ark::Tensor * (ark::Model::*)(ark::Tensor *, const ark::Dims &,
+                                            bool, ark::Tensor *,
+                                            const std::string &)) &
+                 ark::Model::reshape,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("shape"), py::arg("allowzero") = false,
+             py::arg("output") = nullptr, py::arg("name") = "reshape")
+        .def("reshape",
+             (ark::Tensor * (ark::Model::*)(ark::Tensor *,
+                                            std::initializer_list<ark::DimType>,
+                                            bool, ark::Tensor *,
+                                            const std::string &)) &
+                 ark::Model::reshape,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("shape"), py::arg("allowzero") = false,
+             py::arg("output") = nullptr, py::arg("name") = "reshape")
+        .def("identity", &ark::Model::identity,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("deps") = std::vector<ark::Tensor *>(),
+             py::arg("output") = nullptr, py::arg("name") = "identity")
+        .def("sharding", &ark::Model::sharding,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("axis"), py::arg("dim_per_shard"),
+             py::arg("name") = "sharding")
+        .def("reduce", &ark::Model::reduce,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("axis"), py::arg("output") = nullptr,
+             py::arg("is_relu") = false, py::arg("name") = "reduce")
+        .def("transpose", &ark::Model::transpose,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("perm"), py::arg("output") = nullptr,
+             py::arg("name") = "transpose")
+        .def("matmul", &ark::Model::matmul,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("other"), py::arg("output") = nullptr,
+             py::arg("splitk") = 1, py::arg("trans_input") = false,
+             py::arg("trans_other") = false, py::arg("is_relu") = false,
+             py::arg("name") = "matmul", py::arg("gran_lev") = -1)
+        .def("linear", &ark::Model::linear,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("out_features"), py::arg("bias") = true,
+             py::arg("output") = nullptr, py::arg("splitk") = 1,
+             py::arg("is_relu") = false, py::arg("name") = "linear",
+             py::arg("gran_lev") = -1)
+        .def("im2col", &ark::Model::im2col,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("kernel_height"), py::arg("kernel_width"),
+             py::arg("stride_height"), py::arg("stride_width"),
+             py::arg("pad_height"), py::arg("pad_width"),
+             py::arg("dilation_height"), py::arg("dilation_width"),
+             py::arg("output") = nullptr, py::arg("name") = "im2col")
+        .def("conv2d", &ark::Model::conv2d,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("in_channels"), py::arg("out_channels"),
+             py::arg("kernel_size"), py::arg("stride"), py::arg("padding"),
+             py::arg("bias") = false, py::arg("output") = nullptr,
+             py::arg("name") = "conv2d")
+        .def("max_pool", &ark::Model::max_pool,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("kernel_size"), py::arg("stride"),
+             py::arg("output") = nullptr, py::arg("name") = "max_pool")
+        .def("scale", &ark::Model::scale,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("val"), py::arg("output") = nullptr,
+             py::arg("name") = "scale")
+        .def("add", &ark::Model::add,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("other"), py::arg("output") = nullptr,
+             py::arg("name") = "add")
+        .def("mul", &ark::Model::mul,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("other"), py::arg("output") = nullptr,
+             py::arg("name") = "mul")
+        .def("send", &ark::Model::send,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("id"), py::arg("gpu_dst"), py::arg("bytes") = 0,
+             py::arg("output") = nullptr, py::arg("name") = "send")
+        .def("send_done", &ark::Model::send_done,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("id"), py::arg("output") = nullptr,
+             py::arg("name") = "send_done")
+        .def("recv", &ark::Model::recv,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("id"), py::arg("gpu_src"), py::arg("bytes") = 0,
+             py::arg("output") = nullptr, py::arg("name") = "recv")
+        .def("send_mm", &ark::Model::send_mm,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("id"), py::arg("gpu_dst"), py::arg("bytes") = 0,
+             py::arg("output") = nullptr, py::arg("name") = "send_mm")
+        .def("recv_mm", &ark::Model::recv_mm,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("id"), py::arg("gpu_src"), py::arg("bytes") = 0,
+             py::arg("output") = nullptr, py::arg("name") = "recv_mm")
+        .def("all_reduce", &ark::Model::all_reduce,
+             py::return_value_policy::reference_internal, py::arg("input"),
+             py::arg("gpu_id"), py::arg("gpu_num"), py::arg("output") = nullptr,
+             py::arg("name") = "all_reduce");
 }
--- python/python_test.py
+++ python/python_test.py
@@ -6,10 +6,45 @@ a = ark.Dims([1, 2, 3, 4])
 print(ark.NO_DIM)
 print(a[2])
 
-ark.srand(42)  
-  
-random_number = ark.rand()  
+ark.srand(42)
+
+random_number = ark.rand()
 
 print(random_number)
+
+# Create a TensorBuf object
+buf = ark.TensorBuf(1024, 1)
+
+# Create a Dims object for shape, ldims, offs, and pads
+shape = ark.Dims(1, 2, 3, 4)
+ldims = ark.Dims(4, 4, 4, 4)
+offs = ark.Dims(0, 0, 0, 0)
+pads = ark.Dims(1, 1, 1, 1)
+
+# # Create a Tensor object
+tensor = ark.Tensor(shape, ark.TensorType.FP32, buf, ldims,
+                    offs, pads, False, False, 0, "my_tensor")
+
+# # Call Tensor methods
+tensor_size = tensor.size()
+tensor_offset = tensor.offset(1, 2, 3, 4)
+
+print(tensor_size)
+print(tensor_offset)
+
+# Create a Model instance
+model = ark.Model()
+
+# Test the tensor method
+t1 = model.tensor(ark.Dims(1, 2, 3, 4), ark.TensorType.FP32)
+t2 = model.tensor(ark.Dims(1, 2, 3, 4), ark.TensorType.FP32)
+
+scaled_tensor = model.scale(t1, 2.0)
+
+# Test the add method
+added_tensor = model.add(t1, t2)
+
+# Test the mul method
+multiplied_tensor = model.mul(t1, t2)
 
 print("ark test success")
