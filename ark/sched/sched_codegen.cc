3bec92282b75a89a25b33f7a853779864a919492
Lifanwu <v-lifanwu@microsoft.com>
2023-06-25 04:48:15 +0000

    add send_mm to is_tiled_op

--- ark/sched/sched_codegen.cc
+++ ark/sched/sched_codegen.cc
@@ -33,7 +33,10 @@ bool is_tiled_op(const SchedOp &sop)
            (sop.get_op()->type == OP_SCALE) ||
            (sop.get_op()->type == OP_GELU) ||
            (sop.get_op()->type == OP_IM2COL) ||
-           (sop.get_op()->type == OP_TRANSPOSE);
+           (sop.get_op()->type == OP_TRANSPOSE) ||
+           (sop.get_op()->type == OP_SEND_MM) ||
+              (sop.get_op()->type == OP_RECV_MM) 
+           ;
 }
 
 size_t SimpleCodeGenerator::get_tensor_offset(const Tensor *tensor)
