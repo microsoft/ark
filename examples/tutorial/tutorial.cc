c4ef0a383e704a4d7697ee25d4d8b6abdbcc4308
Lifanwu <v-lifanwu@microsoft.com>
2023-06-25 04:45:20 +0000

    backup

--- examples/tutorial/tutorial.cc
+++ examples/tutorial/tutorial.cc
@@ -11,9 +11,9 @@ int main(int argc, const char **argv)
 {
     int N = 1, C = 3, H = 256, W = 256;
     Model model;
-    Tensor input = model.tensor({N, C, H, W}, FP16);
-    Tensor other = model.tensor({N, C, H, W}, FP16);
-    Tensor output = model.add(input, other);
+    Tensor *input = model.tensor({N, C, H, W}, FP16);
+    Tensor *other = model.tensor({N, C, H, W}, FP16);
+    Tensor *output = model.add(input, other);
     Executor exe{0, 0, 1, model, "test_add"};
     exe.compile();
     exe.launch();
