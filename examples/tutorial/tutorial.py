c8b4869dbfc23a6043bd2cb6c968fd5740e26172
Lifanwu <v-lifanwu@microsoft.com>
2023-06-26 10:32:16 +0000

    update the quick start

--- examples/tutorial/tutorial.py
+++ examples/tutorial/tutorial.py
@@ -30,6 +30,9 @@ other_np = np.random.rand(1, 32).astype(np.float16)
 exe.tensor_memcpy_host_to_device(input, input_np)
 exe.tensor_memcpy_host_to_device(other, other_np)
 
+print("input: ", input_np)
+print("other: ", other_np)
+
 # Launch the kernel and run for 1 iteration
 exe.launch()
 exe.run(1)
@@ -41,5 +44,13 @@ exe.stop()
 output_np = np.zeros((1, 32), dtype=np.float16)
 exe.tensor_memcpy_device_to_host(output_np, output)
 
+print("output: ", output_np)
+
 # test if the result is correct
 assert np.allclose(output_np, input_np + other_np)
+
+max_error = np.max(np.abs(output_np - (input_np + other_np)))
+mean_error = np.mean(np.abs(output_np - (input_np + other_np)))
+
+print("max error: ", max_error, "mean error: ", mean_error)
+print("test_add passed")
