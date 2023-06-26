0819e449e0d5de96d5833f45e549c96c861510f7
Lifanwu <v-lifanwu@microsoft.com>
2023-06-26 12:13:34 +0000

    clean up

--- python/megatron/pytorch_test.py
+++ python/megatron/pytorch_test.py
@@ -1,20 +1,20 @@
 import torch
 import torch.distributed as dist
 
-# 初始化分布式进程组
+# initialize the process group
 dist.init_process_group(backend="nccl" if torch.cuda.is_available() else "gloo")
 
-# 创建一个张量并将其放在当前设备上
+# create local tensors on each process
 rank = dist.get_rank()
 size = dist.get_world_size()
 tensor = torch.ones(3).to(rank) * (rank + 1)
 
 print(f"Before all_reduce: Rank {rank}, tensor = {tensor}")
 
-# 执行AllReduce操作
+# perform all-reduce on tensor
 dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
 
 print(f"After all_reduce: Rank {rank}, tensor = {tensor}")
 
-# 结束分布式进程组
+# destroy the process group
 dist.destroy_process_group()
