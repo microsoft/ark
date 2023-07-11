4a97e79595514789eee5de072a72ddb1aea767d9
Lifanwu <v-lifanwu@microsoft.com>
2023-07-11 12:53:15 +0000

    add the allgather inplace test

--- python/unittest/allgather_test.py
+++ python/unittest/allgather_test.py
@@ -6,7 +6,7 @@ import numpy as np
 import multiprocessing
 
 
-def all_gather_test(rank, np_inputs, world_size, tensor_len):
+def all_gather_test_not_inplace(rank, np_inputs, world_size, tensor_len):
     tensor_size = tensor_len * 2
     print("rank:", rank)
 
@@ -15,11 +15,6 @@ def all_gather_test(rank, np_inputs, world_size, tensor_len):
 
     input_tensor = model.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
 
-    output_tensor = model.tensor(
-        ark.Dims(tensor_size * world_size), ark.TensorType.FP16
-    )
-
-    recv_buffers = model.sharding(output_tensor, 0, tensor_size)
     print(input_tensor)
     allgather_result = model.all_gather(input_tensor, rank, world_size)
     print(allgather_result)
@@ -49,7 +44,59 @@ def all_gather_test(rank, np_inputs, world_size, tensor_len):
         print("rank:", rank, "done")
 
 
-def all_gather_test_main(world_size, tensor_len):
+def all_gather_test_inplace(rank, np_inputs, world_size, tensor_len):
+    print("rank:", rank)
+
+    # Create a Model instance
+    model = ark.Model()
+
+    # input_tensor = model.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
+
+    output_tensor = model.tensor(
+        ark.Dims(tensor_len * world_size), ark.TensorType.FP16
+    )
+
+    print("output_tensor_shape", output_tensor.shape())
+
+    output_shard = model.sharding(output_tensor, 0, tensor_len)
+    print("output_tensor_shape", output_tensor.shape())
+
+    input_tensor = output_shard[rank]
+    print(input_tensor)
+    allgather_result = model.all_gather(
+        input_tensor, rank, world_size, output_shard
+    )
+    print(allgather_result)
+    exe = ark.Executor(rank, rank, world_size, model, "test_python_bindings")
+    exe.compile()
+
+    exe.launch()
+    print("rank:", rank, " input_tensor:", np_inputs[rank])
+    exe.tensor_memcpy_host_to_device(input_tensor, np_inputs[rank])
+    exe.run(1)
+    exe.stop()
+    host_output = np.zeros(tensor_len * world_size, dtype=np.float16)
+    # for tensor_shard in range(world_size):
+    # if tensor_shard == rank:
+    # print("local rank: ")
+    print(allgather_result)
+
+    exe.tensor_memcpy_device_to_host(host_output, output_tensor)
+    # return
+    print("host_output:", host_output)
+    # gt = np_inputs[tensor_shard]
+
+    # print("gt:", gt)
+    # max_error = np.max(np.abs(host_output - gt))
+    # mean_error = np.mean(np.abs(host_output - gt))
+    # print("max error:", max_error)
+    # print("mean error:", mean_error)
+    # print("rank:", rank, "done")
+
+
+def all_gather_test_main(
+    world_size, tensor_len, allgather_test_func=all_gather_test_not_inplace
+):
     ark.init()
     num_processes = world_size  # number of processes
     processes = []
@@ -58,7 +105,8 @@ def all_gather_test_main(world_size, tensor_len):
         np_inputs.append(np.random.rand(tensor_len).astype(np.float16))
     for i in range(num_processes):
         process = multiprocessing.Process(
-            target=all_gather_test, args=(i, np_inputs, world_size, tensor_len)
+            target=allgather_test_func,
+            args=(i, np_inputs, world_size, tensor_len),
         )
         process.start()
         processes.append(process)
@@ -68,5 +116,5 @@ def all_gather_test_main(world_size, tensor_len):
 
 
 if __name__ == "__main__":
-    all_gather_test_main(2, 2048)
-    all_gather_test_main(4, 2048)
+    all_gather_test_main(2, 64)
+    all_gather_test_main(3, 64)
