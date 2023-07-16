f3b9650e7c39447977d5041cd7305e095ff40eca
Lifanwu <v-lifanwu@microsoft.com>
2023-07-16 08:56:16 +0000

    add some comment

--- python/unittest/allgather_parallel_test.py
+++ python/unittest/allgather_parallel_test.py
@@ -8,24 +8,28 @@ import multiprocessing
 M, N, K = 64, 128, 8
 
 
-def all_gather_test_not_inplace(rank, np_inputs, world_size):
+# Use all-gather operation to perform tensor parallel matmul
+def all_gather_tensor_parallel(rank, np_inputs, world_size):
     print("rank:", rank)
+
+    # The number of columns per GPU
     N_pergpu = N // world_size
     # Create a Model instance
     model = ark.Model()
 
+    # The input and other tensor of the matmul, note that the other is only a shard of the whole other tensor, we split the other tensor to perform tensor parallel matmul
     input_tensor = model.tensor(ark.Dims(M, K), ark.TensorType.FP16)
     other_tensor_shard = model.tensor(
         ark.Dims(K, N_pergpu), ark.TensorType.FP16
     )
-
+    # The whole output tensor of the matmul
     whole_output_trans = model.tensor(ark.Dims(N, M), ark.TensorType.FP16)
 
-    # shard the other at the first dim
+    # shard the other at the first dim, the rank'th shard is the output of the matmul, and we will perform an inplace all-gather operation to get the whole output tensor
     whole_output_shard = model.sharding(whole_output_trans, 0, N_pergpu)
 
     output_tensor_shard_trans = whole_output_shard[rank]
-    print("output_tensor_shard_trans shape", output_tensor_shard_trans.shape())
+
     # output_tensor_shard = matmul(input, other) => output_tensor_shard.transpose = matmul(other.transpose, input.transpose)
     model.matmul(
         other_tensor_shard,
@@ -35,27 +39,33 @@ def all_gather_test_not_inplace(rank, np_inputs, world_size):
         trans_input=True,
         trans_other=True,
     )
-
+    # In-place all-gather operation to get the whole output tensor
     allgather_result = model.all_gather(
         output_tensor_shard_trans, rank, world_size, whole_output_shard
     )
+
+    # Create an executor instance
     exe = ark.Executor(rank, rank, world_size, model, "test_all_gather")
     exe.compile()
-
     exe.launch()
-    # init the input tensor and other tensor
+
+    # Copy input and other tensors to device
     exe.tensor_memcpy_host_to_device(input_tensor, np_inputs["input"])
     np_other = np_inputs["other"]
     np_other_shard = np.split(np_other, world_size, axis=1)[rank]
     np_other_shard_copy = np_other_shard.copy()
     exe.tensor_memcpy_host_to_device(other_tensor_shard, np_other_shard_copy)
 
+    # Run the executor
     exe.run(1)
     exe.stop()
 
+    # Copy output tensor to host
     output_host_trans = np.zeros((N, M), dtype=np.float16)
     exe.tensor_memcpy_device_to_host(output_host_trans, whole_output_trans)
     output_host = output_host_trans.transpose()
+
+    # Calculate ground truth
     gt = np.matmul(np_inputs["input"], np_other)
 
     max_error = np.max(np.abs(output_host - gt))
@@ -69,7 +79,7 @@ def all_gather_test_not_inplace(rank, np_inputs, world_size):
 
 
 def all_gather_test_main(
-    world_size, allgather_test_func=all_gather_test_not_inplace
+    world_size, allgather_test_func=all_gather_tensor_parallel
 ):
     ark.init()
     num_processes = world_size  # number of processes
@@ -92,5 +102,5 @@ def all_gather_test_main(
 
 
 if __name__ == "__main__":
-    all_gather_test_main(2, all_gather_test_not_inplace)
-    # all_gather_test_main(3, all_gather_test_not_inplace)
+    all_gather_test_main(2, all_gather_tensor_parallel)
+    all_gather_test_main(4, all_gather_tensor_parallel)
