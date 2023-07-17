657d61df3d288b60dfab90c3a6aa5ca92b09f1b2
chhwang <changhohwang@microsoft.com>
2023-07-17 13:19:13 +0000

    Fixes

--- ark/model_test.cc
+++ ark/model_test.cc
@@ -20,8 +20,8 @@ ark::unittest::State test_simple_mm()
     unsigned int batch_size = 1;
 
     ark::Model m;
-    ark::Tensor *input = m.tensor({batch_size, channel, 1, in_dim}, ark::FP16);
-    ark::Tensor *weight = m.tensor({1, in_dim, 1, units}, ark::FP16);
+    ark::Tensor *input = m.tensor({batch_size, channel, in_dim}, ark::FP16);
+    ark::Tensor *weight = m.tensor({in_dim, units}, ark::FP16);
     ark::Tensor *output = m.matmul(input, weight);
 
     return ark::unittest::SUCCESS;
