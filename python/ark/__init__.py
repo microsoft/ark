a4897d3d6720bae0c3056a51ec10ae589ae376f5
Lifanwu <v-lifanwu@microsoft.com>
2023-07-25 08:57:13 +0000

    add a python wrapper

--- python/ark/__init__.py
+++ python/ark/__init__.py
@@ -7,4 +7,17 @@ ark_root = os.environ.get("ARK_ROOT", None)
 if ark_root is None:
     os.environ["ARK_ROOT"] = os.path.abspath(os.path.dirname(__file__))
 
-from ._ark_core import *
+from ._ark_core import init, srand, rand, Dims, Executor, Model, Tensor, TensorBuf, TensorType
+
+__all__ = [
+    "init",
+    "srand",
+    "rand",
+    "Dims",
+    "Executor",
+    "Model",
+    "Tensor",
+    "TensorBuf",
+    "TensorType",
+    "Model",
+]
