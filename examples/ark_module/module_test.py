5e0b9a554d3d217f02633f76a3eaf2f9a3dd95f6
Lifanwu <v-lifanwu@microsoft.com>
2023-07-25 16:24:00 +0000

    add the module test

---
+++ examples/ark_module/module_test.py
@@ -0,0 +1,151 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import torch
+import numpy as np
+import torch.nn as nn
+
+import ark
+
+d_model = 512  # Dimension of word embeddings
+d_ff = 2048  # Dimension of the hidden layer in the feed-forward network
+d_k = d_v = 64  # Dimensions of K(=Q) and V in the attention mechanism
+n_layers = 2  # Number of encoder and decoder layers
+n_heads = 8  # Number of heads in Multi-Head Attention set to 8
+
+batch_size = 1
+seq_len = 64
+src_vocab_size = 128
+
+# The number of input tokens is 10
+# Used for constructing the masks
+input_seq_len = 10
+
+# Megatron-LM on 2 GPU
+num_gpu = 2
+n_heads_per_gpu = n_heads // num_gpu
+
+import ark
+
+
+class PoswiseFeedForwardNet:
+    def __init__(self, model):
+        self.model = model
+        self.weight_1 = model.tensor(
+            ark.Dims(d_model, d_ff), ark.TensorType.FP16
+        )
+        self.weight_2 = model.tensor(
+            ark.Dims(d_ff, d_model), ark.TensorType.FP16
+        )
+
+    def forward(self, inputs):
+        middle_result = self.model.matmul(inputs, self.weight_1, is_relu=True)
+        middle_result1 = self.model.matmul(middle_result, self.weight_2)
+        output = self.model.add(middle_result1, inputs)
+        output_layernorm = self.model.layernorm(output)
+        return output_layernorm
+
+    def init_model(self, param, exe, prefix=""):
+        exe.tensor_memcpy_host_to_device(
+            self.weight_1, param[prefix + "weight_1"]
+        )
+        exe.tensor_memcpy_host_to_device(
+            self.weight_2, param[prefix + "weight_2"]
+        )
+
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.weight_1 = nn.Parameter(torch.FloatTensor(d_model, d_ff))
+        self.weight_2 = nn.Parameter(torch.FloatTensor(d_ff, d_model))
+
+    # inputs: [batch_size, seq_len, d_model]
+    def forward(self, inputs):
+        output = torch.matmul(
+            inputs, self.weight_1
+        )  # [batch_size, seq_len, d_ff]
+        output = nn.ReLU()(output)
+        output = torch.matmul(
+            output, self.weight_2
+        )  # [batch_size, seq_len, d_model]
+        output = nn.LayerNorm(d_model)(
+            output + inputs
+        )  # [batch_size, seq_len, d_model]
+        return output
+
+    def init_model(self, param, prefix=""):
+        self.weight_1.data.copy_(torch.from_numpy(param[prefix + "weight_1"]))
+        self.weight_2.data.copy_(torch.from_numpy(param[prefix + "weight_2"]))
+
+
+def test_PoswiseFeedForwardNet():
+    ark.init()
+
+    # Create a Model instance
+    model = ark.Model()
+
+    input_tensor = model.tensor(
+        ark.Dims(batch_size, seq_len, d_model), ark.TensorType.FP16
+    )
+
+    ark_model = PoswiseFeedForwardNet(model)
+    output_tensor = ark_model.forward(input_tensor)
+    # Test the mul method
+    exe = ark.Executor(0, 0, 1, model, "test_PoswiseFeedForwardNet")
+    exe.compile()
+    input_tensor_host = (
+        (np.random.rand(batch_size, seq_len, d_model) - 0.5) * 0.1
+    ).astype(np.float16)
+
+    exe.launch()
+    exe.tensor_memcpy_host_to_device(input_tensor, input_tensor_host)
+
+    weight_1_host = ((np.random.rand(d_model, d_ff) - 0.5) * 0.1).astype(
+        np.float16
+    )
+    weight_2_host = ((np.random.rand(d_ff, d_model) - 0.5) * 0.1).astype(
+        np.float16
+    )
+
+    param = {"weight_1": weight_1_host, "weight_2": weight_2_host}
+
+    ark_model.init_model(param, exe)
+
+    exe.run(1)
+    exe.stop()
+
+    output_tensor_host = np.zeros(
+        (batch_size, seq_len, d_model), dtype=np.float16
+    )
+
+    exe.tensor_memcpy_device_to_host(output_tensor_host, output_tensor)
+
+    input_tensor_host_float32 = input_tensor_host.astype(np.float32)
+
+    torch_input = torch.from_numpy(input_tensor_host_float32)
+
+    torch_model = PoswiseFeedForwardNet()
+
+    torch_model.init_model(param)
+
+    gt = torch_model(torch_input).detach().numpy().astype(np.float16)
+
+    # test if the result is correct
+    max_error = np.max(np.abs(output_tensor_host - gt))
+    avg_error = np.mean(np.abs(output_tensor_host - gt))
+    # print(input_tensor_host)
+    # print(output_tensor_host)
+    # print(gt)
+    print("poswise feed forward net test")
+    print(
+        "batch_size:",
+        batch_size,
+        "seq_len:",
+        seq_len,
+        "d_model:",
+        d_model,
+        "d_ff:",
+        d_ff,
+    )
+    print("max error: ", max_error, "avg error: ", avg_error)
--- python/ark/module.py
+++ python/ark/module.py
@@ -24,7 +24,16 @@ class Module:
     def register_parameter(self, name: str, param: Optional['Tensor']) -> None:
         self._parameters[name] = param
 
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if isinstance(__value, Module):
+            self.register_module(__name, __value)
+        elif isinstance(__value, Tensor):
+            self.register_parameter(__name, __value)
+        else:
+            super().__setattr__(__name, __value)
+
     # Loads a model from a state_dict and copy the parameters to the device GPU.
+    # Must be called after the executor is launched.
     def load_state_dict(self,executor, state_dict, prefix=''):
         for name, module in self._modules.items():
             if module is not None:
@@ -33,7 +42,8 @@ class Module:
             executor.tensor_memcpy_host_to_device(param, state_dict[prefix + name])
 
     # Copies the parameters from the device GPU to the host and saves the model to a state_dict.
-    def state_dict(self,executor):
+    # Must be called after the executor is launched.
+    def state_dict(self, executor):
         state_dict = {}
         for name, module in self._modules.items():
             if module is not None:
