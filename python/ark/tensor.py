491b272641c7f70d9f4193a660d49efbde686f13
Lifanwu <v-lifanwu@microsoft.com>
2023-07-26 09:23:54 +0000

    add the tensor init function

--- python/ark/tensor.py
+++ python/ark/tensor.py
@@ -6,28 +6,25 @@ from .executor import Executor
 import numpy as np
 
 class Tensor(_Tensor):
-    def __init__(self, np_tensor):
-        self.np_tensor = np_tensor
-
     def shape(self):
         ark_dims = super().shape()
         shape = []
         for i in range(ark_dims.ndims()):
             shape.append(ark_dims[i])
         return shape
-    def to(self, device):
+
+    def to(self, device, np_tensor=None):
         if device == "cpu":
             return self.cpu()
         elif device == "cuda":
-            if self.np_tensor == None:
-                raise RuntimeError("Tensor is not initiated with a numpy array, cannot move to cuda")
-            return self.cuda()
+            if np_tensor == None:
+                raise RuntimeError("Usage: tensor.to('cuda', np_tensor)")
+            return self.initiate(np_tensor)
         else:
             raise RuntimeError("Unknown device: " + device, "Only cpu and cuda are supported")
     def cpu(self):
         np_array = np.empty(self.shape(), dtype=np.float16)
         return Executor.get_executor().tensor_memcpy_device_to_host(np_array, self)
-    
-    def cuda(self):
-        return Executor.get_executor().tensor_memcpy_host_to_device(self, self.np_tensor)
 
+    def initiate(self, np_tensor):
+        return Executor.get_executor().tensor_memcpy_host_to_device(self, np_tensor)
