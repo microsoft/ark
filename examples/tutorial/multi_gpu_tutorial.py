0e44c04b4914677aae6e5ddee956b75329289d9e
Lifanwu <v-lifanwu@microsoft.com>
2023-07-31 11:08:35 +0000

    update the tutorial

--- examples/tutorial/multi_gpu_tutorial.py
+++ examples/tutorial/multi_gpu_tutorial.py
@@ -13,56 +13,53 @@ tensor_size = tensor_len * 2
 
 def sendrecv_test_ping_pong_function(rank, np_inputs):
     print("rank:", rank)
-    # Create a Model instance
-    model = ark.Model(rank)
+    # Initialize the ARK model
+    ark.init_model(rank, world_size)
 
     # Define the behavior for rank 0
     if rank == 0:
-        send_tensor = model.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
-        recv_tensor = model.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
+        send_tensor = ark.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
+        recv_tensor = ark.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
 
         # send the tensor to rank 1
         send_id, dst_rank = 0, 1
-        model.send(send_tensor, send_id, dst_rank, tensor_size)
-        model.send_done(send_tensor, send_id, dst_rank)
+        ark.send(send_tensor, send_id, dst_rank, tensor_size)
+        ark.send_done(send_tensor, send_id, dst_rank)
 
         # recv the tensor from rank 1
         recv_id, recv_rank = 1, 1
-        model.recv(recv_tensor, recv_id, recv_rank)
+        ark.recv(recv_tensor, recv_id, recv_rank)
 
     # Define the behavior for rank 1
     if rank == 1:
         # recv the tensor from rank 0
-        recv_tensor = model.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
+        recv_tensor = ark.tensor(ark.Dims(tensor_len), ark.TensorType.FP16)
         recv_id, recv_rank = 0, 0
-        recv_dep = model.recv(recv_tensor, recv_id, recv_rank)
+        recv_dep = ark.recv(recv_tensor, recv_id, recv_rank)
 
         # The send must be executed after the recv, in the current scheduler,
         # in one depth their will be send operation, compute operation and
         # recv operation
-        send_tensor = model.identity(recv_tensor, [recv_dep])
+        send_tensor = ark.identity(recv_tensor, [recv_dep])
 
         # Send the received tensor back to rank 0 after an identity operation
         send_id, dst_rank = 1, 0
-        model.send(send_tensor, send_id, dst_rank, tensor_size)
-        model.send_done(send_tensor, send_id, dst_rank)
+        ark.send(send_tensor, send_id, dst_rank, tensor_size)
+        ark.send_done(send_tensor, send_id, dst_rank)
 
-    # Create an executor for the model
-    exe = ark.Executor(rank, rank, world_size, model, "test_sendrecv_ping_pong")
-    exe.compile()
+    # Launch the ARK runtime
+    ark.launch()
 
-    # Launch the execution and perform data transfers
-    exe.launch()
     # Copy send data to GPU0
     if rank == 0:
-        exe.tensor_memcpy_host_to_device(send_tensor, np_inputs)
+        ark.tensor_memcpy_host_to_device(send_tensor, np_inputs)
 
-    exe.run(1)
-    exe.stop()
+    # Run the ARK program
+    ark.run()
 
     # Copy data back to host and calculate errors
     host_output = np.zeros(tensor_len, dtype=np.float16)
-    exe.tensor_memcpy_device_to_host(host_output, recv_tensor)
+    ark.tensor_memcpy_device_to_host(host_output, recv_tensor)
 
     # Print output and error information
     print("host_output:", host_output)
