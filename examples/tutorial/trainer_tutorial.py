6e45d87f106e6201d66c1c4a8ab5e8f63197d6fe
Lifanwu <v-lifanwu@microsoft.com>
2023-08-01 09:29:46 +0000

    update the trainer

--- examples/tutorial/trainer_tutorial.py
+++ examples/tutorial/trainer_tutorial.py
@@ -7,11 +7,10 @@ import torch.nn as nn
 import ark
 import unittest
 
-d_model = 512
-d_ff = 2048
+d_model = 64
+d_ff = 128
 
 batch_size = 1
-seq_len = 64
 
 import ark
 
@@ -25,52 +24,75 @@ class Optimizer:
         if module == None:
             module = self.module
         print("step module: ", module)
-        for param in module.parameters:
-            print("param: ", param, "grads: ", module.grads)
-            grads = module.grads[param]
+        for param_name in module.parameters:
+            print("param_name: ", param_name, "grads: ", module.grads)
+            param = module.parameters[param_name]
+            grads = module.grads[param_name]
+            print("grads shape: ", grads.shape)
+            print("param shape: ", param.shape)
+            grads = ark.reshape(grads, param.shape)
             grads_scale = ark.scale(grads, -1.0 * self.lr)
             param_identity = ark.identity(param)
+            print(
+                param.shape,
+                grads.shape,
+                grads_scale.shape,
+                param_identity.shape,
+            )
             ark.add(param, grads_scale, param_identity)
         for sub_module_name in module.sub_modules:
+            print("sub_module_name: ", sub_module_name)
             self.step(module.sub_modules[sub_module_name])
 
 
 class loss_fn(ark.Module):
     def __init__(self):
-        pass
-        # self.loss = ark.tensor(ark.Dims(1), ark.TensorType.FP16)
+        self.other_parameter = {}
 
     def forward(self, outputs: ark.Tensor, labels: ark.Tensor):
         neg_ground_truth = ark.scale(labels, -1.0)
         diff = ark.add(outputs, neg_ground_truth)
+        self.other_parameter["diff"] = diff
         diff1 = ark.scale(diff, 1.0)
         mse = ark.mul(diff, diff1)
+        # If batch_size is larger than 1, we need to sum the loss over the batch dimension.
+        if batch_size > 1:
+            mse = ark.reduce_sum(mse, axis=0)
+        mse = ark.reshape(mse, outputs.shape[1:])
         return mse
 
     def backward(self, loss):
-        grad_diff = ark.scale(loss, 2.0)
+        diff = self.other_parameter["diff"]
+        grad_diff = ark.reshape(diff, diff.shape[1:])
         return grad_diff
 
 
 class fully_connected_layer(ark.Module):
     def __init__(self, input_size, output_size):
         super(fully_connected_layer, self).__init__()
-        self.weight = ark.tensor(
-            ark.Dims(input_size, output_size), ark.TensorType.FP16
-        )
-        self.bias = ark.tensor(ark.Dims(output_size), ark.TensorType.FP16)
+        self.weight = ark.tensor([input_size, output_size], ark.TensorType.FP16)
+        self.bias = ark.tensor([1, output_size], ark.TensorType.FP16)
+        self.other_parameter = {}
 
     def forward(self, inputs):
+        self.other_parameter["inputs"] = inputs
         output = ark.matmul(inputs, self.weight)
         output = ark.add(output, self.bias)
+        print("output: ", output.shape)
         return output
 
-    def backward(self, loss):
-        grad_bias = ark.scale(loss, 1.0)
-        grad_weight = ark.matmul(loss, self.weight, transpose_a=True)
-        grad_input = ark.matmul(loss, self.weight, transpose_b=True)
-        self.grads['weight'] = grad_weight
-        self.grads['bias'] = grad_bias
+    def backward(self, grads_output):
+        grad_bias = grads_output
+        inputs = self.other_parameter["inputs"]
+        print("inputs: ", inputs.shape, "grads_output: ", grads_output.shape)
+        grad_weight = ark.matmul(inputs, grads_output, transpose_a=True)
+        grad_input = ark.matmul(grads_output, self.weight, transpose_b=True)
+        print("grad_weight: ", grad_weight.shape)
+        print("self.weight: ", self.weight.shape)
+        grad_weight = ark.reshape(grad_weight, self.weight.shape)
+        grad_bias = ark.reshape(grad_bias, self.bias.shape)
+        self.grads["weight"] = grad_weight
+        self.grads["bias"] = grad_bias
         return grad_input
 
 
@@ -101,15 +123,13 @@ class Trainer:
         self.module = module
         self.loss_fn = loss_fn
         self.optimizer = optimizer
-        self.input = ark.tensor(
-            [batch_size, seq_len, d_model], ark.TensorType.FP16
-        )
-        self.label = ark.tensor(
-            [batch_size, seq_len, d_model], ark.TensorType.FP16
-        )
+        self.input = ark.tensor([batch_size, d_model], ark.TensorType.FP16)
+        self.label = ark.tensor([batch_size, d_model], ark.TensorType.FP16)
         output = self.module(self.input)
         loss = self.loss_fn(output, self.label)
-        self.loss_fn.backward(loss)
+        grad_loss = self.loss_fn.backward(loss)
+        print("grad_loss: ", grad_loss.shape)
+        self.module.backward(grad_loss)
         self.optimizer.step()
 
     def trainer_init(self, inputs, labels):
@@ -131,7 +151,7 @@ def test_TestModel():
     ark.init_model()
 
     input_tensor = ark.tensor(
-        ark.Dims(batch_size, seq_len, d_model), ark.TensorType.FP16
+        [batch_size, d_model], ark.TensorType.FP16
     )
     ark_model = TestModelARK()
     trainer = Trainer(ark_model, loss_fn(), Optimizer(ark_model, 0.001))
