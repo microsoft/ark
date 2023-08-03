564fee93b7e36a9d560a3912d6162b703678e10b
Lifanwu <v-lifanwu@microsoft.com>
2023-08-03 11:51:46 +0000

    update

--- examples/tutorial/module_tutorial.py
+++ examples/tutorial/module_tutorial.py
@@ -92,7 +92,7 @@ def module_test():
     ).astype(np.float16)
     input_tensor.memcpy(input_tensor_host, "host_to_device")
     # The following line has the same effect as the above line
-    # ark.tensor_memcpy_host_to_device(input_tensor, input_tensor_host) 
+    # ark.tensor_memcpy_host_to_device(input_tensor, input_tensor_host)
 
     # Initialize the parameters of the ARK module using numpy state_dict
     weight_1_host = ((np.random.rand(d_model, d_ff) - 0.5) * 0.1).astype(
