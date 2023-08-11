d713ae716153193b4488348d317ce7ff524cfe5b
Changho Hwang <changhohwang@microsoft.com>
2023-08-11 18:41:38 +0800

    Update GpuCommSw interfaces (#81)

--- ark/gpu/gpu_comm_sw.h
+++ ark/gpu/gpu_comm_sw.h
@@ -4,39 +4,18 @@
 #ifndef ARK_GPU_COMM_SW_H_
 #define ARK_GPU_COMM_SW_H_
 
-#include <list>
 #include <map>
 #include <memory>
-#include <set>
 #include <string>
-#include <sys/mman.h>
-#include <thread>
 #include <vector>
 
-#include "gpu/gpu_buf.h"
-#include "gpu/gpu_common.h"
-#include "ipc/ipc_socket.h"
-#include "net/net_ib.h"
+#include "gpu_mem.h"
+#include "gpu_buf.h"
+#include "gpu_common.h"
 
 namespace ark {
 
-//
-struct GpuCommInfo
-{
-    NetIbMr::Info sid_mris[MAX_NUM_SID];
-    uint64_t sid_offs[MAX_NUM_SID];
-    NetIbQp::Info qpi;
-    uint64_t bytes;
-};
-
-//
-struct GpuSendRecvInfo
-{
-    int sid;
-    int remote_rank;
-    std::size_t bytes;
-    bool is_recv;
-};
+class GpuBuf;
 
 //
 class GpuCommSw
@@ -50,54 +29,24 @@ class GpuCommSw
                       bool is_recv);
     void configure(std::vector<std::pair<int, size_t>> &export_sid_offs,
                    std::map<int, std::vector<GpuBuf *>> &import_gid_bufs);
+
     void import_buf(const int gid, GpuBuf *buf);
 
-    void request_loop();
     void launch_request_loop();
+
     void stop_request_loop();
 
     void set_request(const Request &db);
 
     GpuMem *get_data_mem(const int gid);
-    GpuMem *get_sc_rc_mem(const int gid);
-    IpcMem *get_info(const int gid);
-    GpuPtr get_request_ref() const;
-    bool is_using_ib() const
-    {
-        return this->net_ib_mgr != nullptr;
-    }
 
-  private:
-    //
-    const std::string name;
-    //
-    const int gpu_id;
-    const int rank;
-    const int world_size;
-    //
-    std::list<std::unique_ptr<GpuMem>> remote_data_mems_storage;
-    std::list<std::unique_ptr<GpuMem>> remote_sc_rc_mems_storage;
-    std::list<std::unique_ptr<IpcMem>> infos_storage;
-    //
-    std::vector<GpuMem *> data_mems;
-    //
-    std::vector<GpuMem *> sc_rc_mems;
-    //
-    std::vector<IpcMem *> infos;
-    //
-    std::vector<std::vector<GpuPtr>> addr_table;
-    //
-    Request *request = nullptr;
-    std::thread *request_loop_thread = nullptr;
-    volatile bool run_request_loop_thread = false;
-    //
-    IpcSocket *ipc_socket = nullptr;
-    //
-    NetIbMgr *net_ib_mgr = nullptr;
-    std::vector<NetIbMr *> sid_mrs;
-    std::map<int, NetIbQp *> qps;
-    std::vector<GpuSendRecvInfo> send_recv_infos;
-    std::map<int, std::vector<NetIbMr::Info>> mris;
+    GpuPtr get_request_ref() const;
+
+    bool is_using_ib() const;
+
+  protected:
+    class Impl;
+    std::unique_ptr<Impl> impl;
 };
 
 } // namespace ark
--- ark/gpu/gpu_compile.cc
+++ ark/gpu/gpu_compile.cc
@@ -222,12 +222,14 @@ const string gpu_compile(const vector<string> &codes,
             if (max_reg_cnt > 0) {
                 exec_cmd << "-maxrregcount " << max_reg_cnt << " ";
             }
+            stringstream include_args;
             // clang-format off
+            include_args << "-I" << ark_root << "/include "
+                         << "-I" << ark_root << "/include/kernels ";
             exec_cmd << "-ccbin g++ -std c++17 -lcuda "
                 "--define-macro=ARK_TARGET_CUDA_ARCH=" << arch << " "
-                "--define-macro=ARK_COMM_SW=" << (int)use_comm_sw << " "
-                "-I" << ark_root << "/include "
-                "-I" << ark_root << "/include/kernels "
+                "--define-macro=ARK_COMM_SW=" << (int)use_comm_sw << " " <<
+                include_args.str() <<
                 "-gencode arch=compute_" << arch
                 << ",code=sm_" << arch << " "
                 "-o " << item.second << ".cubin "
