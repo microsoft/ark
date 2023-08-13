d1b80296f7a787a6e387b64c5239279304edde56
Lifanwu <v-lifanwu@microsoft.com>
2023-08-13 08:55:13 +0000

    add a bugreproduce

---
+++ examples/llama/bugreproduce.py
@@ -0,0 +1,26 @@
+import ark
+import numpy as np
+
+runtime = ark.Runtime()
+
+input = ark.tensor([1, 64, 64], dtype=ark.FP32)
+
+reduce = ark.reduce_mean(input, axis=input.ndims() - 1)
+
+other = ark.tensor([1, 64, 64], dtype=ark.FP32)
+
+output = ark.add(reduce, other)
+
+runtime.launch()
+
+input_numpy = np.ones([1, 64, 64], dtype=np.float32)
+other_numpy = np.ones([1, 64, 64], dtype=np.float32)
+
+input.from_numpy(input_numpy)
+
+other.from_numpy(other_numpy)
+
+runtime.run()
+
+print(output.to_numpy())
+print(reduce.to_numpy())
