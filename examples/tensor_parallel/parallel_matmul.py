3d273ef53923c559e83e9f19d5c1132545e05b74
Youngwoo LEE <ywlee@kaist.ac.kr>
2023-08-14 22:09:58 +0900

    modified parallel_matmul.py by chhwang

--- examples/tensor_parallel/parallel_matmul.py
+++ examples/tensor_parallel/parallel_matmul.py
@@ -3,83 +3,95 @@ import numpy as np
 import multiprocessing as mp
 import time
 
-C_DIM = 1024
+C_DIM = 8192
 
 
 def main(rank, mat_b_data):
-    model = ark.Model(rank=rank)
+    rt = ark.Runtime(rank=rank, world_size=2)
 
     if rank == 0:
-        mat_a = model.tensor(ark.Dims(128, C_DIM), ark.FP16)
-        mat_b = model.tensor(ark.Dims(C_DIM, C_DIM), ark.FP16)
+        mat_a = ark.tensor([256, C_DIM], ark.FP16)
+        mat_b = ark.tensor([C_DIM, C_DIM], ark.FP16)
 
-        mat_a_part0, mat_a_part1 = model.sharding(
-            mat_a, axis=0, dim_per_shard=64
+        mat_a_part0, mat_a_part1 = ark.sharding(
+            mat_a, axis=0, dim_per_shard=128
         )
-        assert mat_a_part0.shape == [64, C_DIM]
-        assert mat_a_part1.shape == [64, C_DIM]
+        assert mat_a_part0.shape == [128, C_DIM]
+        assert mat_a_part1.shape == [128, C_DIM]
 
-        send_tensor = model.send(
+        send_tensor = ark.send(
             mat_a_part1, id=0, dst_rank=1, bytes=mat_a_part1.shape_bytes()
         )
-        mat_a_part0 = model.identity(mat_a_part0, [send_tensor])
+        mat_a_part0 = ark.identity(mat_a_part0, [send_tensor])
 
-        output = model.tensor(ark.Dims(128, C_DIM), ark.FP16)
-        output_part0, output_part1 = model.sharding(
-            output, axis=0, dim_per_shard=64
+        output = ark.tensor([256, C_DIM], ark.FP16)
+        output_part0, output_part1 = ark.sharding(
+            output, axis=0, dim_per_shard=128
         )
 
-        matmul_part0 = model.matmul(mat_a_part0, mat_b, output=output_part0)
-        output_part1 = model.identity(output_part1, [matmul_part0])
+        matmul_part0 = ark.matmul(mat_a_part0, mat_b, output=output_part0)
+        output_part1 = ark.identity(output_part1, [matmul_part0])
 
-        recv_tensor = model.recv(
+        recv_tensor = ark.recv(
             output_part1, id=1, src_rank=1, bytes=output_part1.shape_bytes()
         )
-    else:
-        mat_a_part1 = model.tensor(ark.Dims(64, C_DIM), ark.FP16)
-        mat_b = model.tensor(ark.Dims(C_DIM, C_DIM), ark.FP16)
+        send_tensor = ark.identity(send_tensor, [recv_tensor])
+        send_done_tensor = ark.send_done(send_tensor, id=0, dst_rank=1)
 
-        recv_tensor = model.recv(
+        mat_c = ark.matmul(mat_a, mat_b)
+    else:
+        mat_a_part1 = ark.tensor([128, C_DIM], ark.FP16)
+        mat_b = ark.tensor([C_DIM, C_DIM], ark.FP16)
+
+        recv_tensor = ark.recv(
             mat_a_part1, id=0, src_rank=0, bytes=mat_a_part1.shape_bytes()
         )
-        mat_a_part1 = model.identity(mat_a_part1, [recv_tensor])
-        matmul_part1 = model.matmul(mat_a_part1, mat_b)
+        mat_a_part1 = ark.identity(mat_a_part1, [recv_tensor])
+        matmul_part1 = ark.matmul(mat_a_part1, mat_b)
 
-        send_tensor = model.send(
+        send_tensor = ark.send(
             matmul_part1, id=1, dst_rank=0, bytes=matmul_part1.shape_bytes()
         )
-        send_done_tensor = model.send_done(send_tensor, id=1, dst_rank=0)
+        send_done_tensor = ark.send_done(send_tensor, id=1, dst_rank=0)
 
-    exe = ark.Executor(rank, rank, 2, model, "parallel_matmul")
-    exe.compile()
+    rt.launch()
 
     if rank == 0:
-        mat_a_data = np.random.rand(128, C_DIM).astype(np.float16)
-        exe.tensor_memcpy_host_to_device(mat_a, mat_a_data)
-    exe.tensor_memcpy_host_to_device(mat_b, mat_b_data)
+        mat_a_data = np.random.uniform(
+            low=-1.0, high=1.0, size=(256, C_DIM)
+        ).astype(np.float16)
+        mat_a.from_numpy(mat_a_data)
+    mat_b.from_numpy(mat_b_data)
 
-    exe.launch()
-    exe.run(1)
-    exe.stop()
+    rt.run(1)
 
     print(f"rank {rank} done")
 
     if rank == 0:
-        result = np.zeros((128, C_DIM), dtype=np.float16)
-        exe.tensor_memcpy_device_to_host(result, output)
+        result = output.to_numpy()
+        ground_truth = mat_c.to_numpy()
+        numeric_epsilon = np.finfo(np.float16).eps
+        atol = 2 * numeric_epsilon * C_DIM
 
-        ground_truth = np.matmul(mat_a_data, mat_b_data)
-        numeric_epsilon_half = np.finfo(np.float16).eps
-        atol = 2 * numeric_epsilon_half * C_DIM
-        np.testing.assert_allclose(result, ground_truth, atol=atol)
+        for i in range(256):
+            try:
+                np.testing.assert_allclose(
+                    result[i], ground_truth[i], rtol=0.01, atol=atol
+                )
+            except AssertionError as e:
+                print(f"i={i}")
+                print(mat_a_part1.to_numpy())
+                raise
 
-    time.sleep(5)
+        print("correctness check passed")
 
 
 if __name__ == "__main__":
     ark.init()
 
-    mat_b_data = np.random.rand(C_DIM, C_DIM).astype(np.float16)
+    mat_b_data = np.random.uniform(
+        low=-1.0, high=1.0, size=(C_DIM, C_DIM)
+    ).astype(np.float16)
     proc0 = mp.Process(target=main, args=(0, mat_b_data))
     proc1 = mp.Process(target=main, args=(1, mat_b_data))
     proc0.start()
