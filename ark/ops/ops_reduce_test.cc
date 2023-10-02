cb68c26a42ff59aae76fd91a7674a1d3f5c5d902
Changho Hwang <changhohwang@microsoft.com>
2023-10-02 07:48:59 +0000

    revert ops_reduce_test

--- ark/ops/ops_reduce_test.cc
+++ ark/ops/ops_reduce_test.cc
@@ -24,12 +24,12 @@ void baseline_reduce_sum_axis0(std::vector<void *> &outputs,
     for (ark::DimType c = 0; c < ish[1]; ++c) {
         for (ark::DimType h = 0; h < ish[2]; ++h) {
             for (ark::DimType w = 0; w < ish[3]; ++w) {
-                float sum = 0;
+                T sum = 0;
                 for (ark::DimType n = 0; n < ish[0]; ++n) {
-                    sum += float(input[n * ish[1] * ish[2] * ish[3] +
-                                       c * ish[2] * ish[3] + h * ish[3] + w]);
+                    sum += input[n * ish[1] * ish[2] * ish[3] +
+                                 c * ish[2] * ish[3] + h * ish[3] + w];
                 }
-                out[c * osh[2] * osh[3] + h * osh[3] + w] = T(sum);
+                out[c * osh[2] * osh[3] + h * osh[3] + w] = sum;
             }
         }
     }
@@ -52,12 +52,12 @@ void baseline_reduce_sum_axis1(std::vector<void *> &outputs,
     for (ark::DimType n = 0; n < ish[0]; ++n) {
         for (ark::DimType h = 0; h < ish[2]; ++h) {
             for (ark::DimType w = 0; w < ish[3]; ++w) {
-                float sum = 0;
+                T sum = 0;
                 for (ark::DimType c = 0; c < ish[1]; ++c) {
-                    sum += float(input[n * ish[1] * ish[2] * ish[3] +
-                                       c * ish[2] * ish[3] + h * ish[3] + w]);
+                    sum += input[n * ish[1] * ish[2] * ish[3] +
+                                 c * ish[2] * ish[3] + h * ish[3] + w];
                 }
-                out[n * osh[1] * osh[2] * osh[3] + h * osh[3] + w] = T(sum);
+                out[n * osh[1] * osh[2] * osh[3] + h * osh[3] + w] = sum;
             }
         }
     }
@@ -80,13 +80,13 @@ void baseline_reduce_sum_axis2(std::vector<void *> &outputs,
     for (ark::DimType n = 0; n < ish[0]; ++n) {
         for (ark::DimType c = 0; c < ish[1]; ++c) {
             for (ark::DimType w = 0; w < ish[3]; ++w) {
-                float sum = 0;
+                T sum = 0;
                 for (ark::DimType h = 0; h < ish[2]; ++h) {
-                    sum += float(input[n * ish[1] * ish[2] * ish[3] +
-                                       c * ish[2] * ish[3] + h * ish[3] + w]);
+                    sum += input[n * ish[1] * ish[2] * ish[3] +
+                                 c * ish[2] * ish[3] + h * ish[3] + w];
                 }
                 out[n * osh[1] * osh[2] * osh[3] + c * osh[2] * osh[3] + w] =
-                    T(sum);
+                    sum;
             }
         }
     }
@@ -109,13 +109,13 @@ void baseline_reduce_sum_axis3(std::vector<void *> &outputs,
     for (ark::DimType n = 0; n < ish[0]; ++n) {
         for (ark::DimType c = 0; c < ish[1]; ++c) {
             for (ark::DimType h = 0; h < ish[2]; ++h) {
-                float sum = 0;
+                T sum = 0;
                 for (ark::DimType w = 0; w < ish[3]; ++w) {
-                    sum += float(input[n * ish[1] * ish[2] * ish[3] +
-                                       c * ish[2] * ish[3] + h * ish[3] + w]);
+                    sum += input[n * ish[1] * ish[2] * ish[3] +
+                                 c * ish[2] * ish[3] + h * ish[3] + w];
                 }
                 out[n * osh[1] * osh[2] * osh[3] + c * osh[2] * osh[3] +
-                    h * osh[3]] = T(sum);
+                    h * osh[3]] = sum;
             }
         }
     }
@@ -192,7 +192,7 @@ ark::unittest::State test_reduce_fp16()
 {
     {
         ark::Model m;
-        ark::Tensor *t = m.tensor(ark::Dims(7, 2, 4, 8192), ark::FP16);
+        ark::Tensor *t = m.tensor(ark::Dims(7, 2, 4, 1024), ark::FP16);
         ark::Tensor *out = m.reduce_sum(t, /*axis=*/0);
 
         auto result = ark::op_test("reduce_fp16_axis0", m, {t}, {out},
@@ -202,7 +202,7 @@ ark::unittest::State test_reduce_fp16()
     }
     {
         ark::Model m;
-        ark::Tensor *t = m.tensor(ark::Dims(7, 2, 4, 8192), ark::FP16);
+        ark::Tensor *t = m.tensor(ark::Dims(7, 2, 4, 1024), ark::FP16);
         ark::Tensor *out = m.reduce_sum(t, /*axis=*/3);
 
         auto result = ark::op_test("reduce_fp16_axis3", m, {t}, {out},
