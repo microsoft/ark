0af1d03913e4b3d15324ba22fa71b32d80b88515
Changho Hwang <changhohwang@microsoft.com>
2023-12-14 13:03:06 +0000

    beautify

--- ark/include/kernels/common/device.h
+++ ark/include/kernels/common/device.h
@@ -8,16 +8,15 @@
 static_assert(false, "Multiple GPU architectures");
 #endif  // defined(ARK_TARGET_CUDA_ARCH) && defined(ARK_TARGET_ROCM_ARCH)
 
-#if !defined(ARK_TARGET_CUDA_ARCH) && !defined(ARK_TARGET_ROCM_ARCH)
-static_assert(false, "Unknown GPU architecture");
-#define ARK_TARGET_CUDA_ARCH // Dummy define
-#endif  // !defined(ARK_TARGET_CUDA_ARCH) && !defined(ARK_TARGET_ROCM_ARCH)
-
-
 #if defined(ARK_TARGET_ROCM_ARCH)
 #include <hip/hip_runtime.h>
 #endif  // !defined(ARK_TARGET_CUDA_ARCH)
 
+#if !defined(ARK_TARGET_CUDA_ARCH) && !defined(ARK_TARGET_ROCM_ARCH)
+static_assert(false, "Unknown GPU architecture");
+#define ARK_TARGET_CUDA_ARCH 800  // Dummy define
+#include <cuda_runtime.h>         // Dummy include
+#endif  // !defined(ARK_TARGET_CUDA_ARCH) && !defined(ARK_TARGET_ROCM_ARCH)
 
 #define DEVICE __forceinline__ __device__
 
