73f5e295d3d9e2475863150192fba20d5939f7a6
Binyang Li <binyli@microsoft.com>
2023-12-19 09:09:18 +0000

    improve comm perf

--- ark/include/kernels/comm.h
+++ ark/include/kernels/comm.h
@@ -136,11 +136,11 @@ DEVICE void device_sync(int, int) {
 template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
           unsigned int NPeers, unsigned int Rank, unsigned long long Offset,
           unsigned long long Length>
-DEVICE void read_and_reduce(size_t src_offset_0, size_t src_offset_1,
-                            size_t src_offset_2, size_t src_offset_3,
-                            size_t src_offset_4, size_t src_offset_5,
-                            size_t src_offset_6, ark::fp16 *src, int uop_idx,
-                            int) {
+DEVICE void ring_read_and_reduce(size_t src_offset_0, size_t src_offset_1,
+                                 size_t src_offset_2, size_t src_offset_3,
+                                 size_t src_offset_4, size_t src_offset_5,
+                                 size_t src_offset_6, ark::fp16 *src,
+                                 int uop_idx, int) {
     // treat channel dst as src since we read from it, and reduce to local
     // memory
     using UnitOp = UnitOp<Dims, Shape, UnitOutDims, NumWarps, 0>;
@@ -171,13 +171,75 @@ DEVICE void read_and_reduce(size_t src_offset_0, size_t src_offset_1,
     }
 }
 
+// Do reduce scatter in a single node with AMD
+template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
+          unsigned int NPeers, unsigned int Rank, unsigned long long Offset,
+          unsigned long long Length>
+DEVICE void parallel_read_and_reduce(size_t src_offset_0, size_t src_offset_1,
+                                     size_t src_offset_2, size_t src_offset_3,
+                                     size_t src_offset_4, size_t src_offset_5,
+                                     size_t src_offset_6, ark::fp16 *src,
+                                     int uop_idx, int) {
+    // treat channel dst as src since we read from it, and reduce to local
+    // memory
+    using UnitOp = UnitOp<Dims, Shape, UnitOutDims, NumWarps, 0>;
+    constexpr int total_tiles =
+        math::div_up<Shape::NCHW, UnitOutDims::NCHW>::value;
+    constexpr int total_threads = total_tiles * UnitOp::NumThreads;
+    constexpr size_t nInt4 = Length / sizeof(int4);
+    const int tid = uop_idx * UnitOp::NumThreads + UnitOp::thread_id();
+    BytesPack<16> *dst =
+        reinterpret_cast<BytesPack<16> *>((uint8_t *)src + Offset);
+    size_t peer_offsets[] = {src_offset_0, src_offset_1, src_offset_2,
+                             src_offset_3, src_offset_4, src_offset_5,
+                             src_offset_6};
+    for (int idx = tid; idx < nInt4; idx += total_threads) {
+        BytesPack<16> tmp = dst[idx];
+        for (int i = 0; i < NPeers; ++i) {
+            int chan_idx = (Rank + i) % NPeers;
+            const size_t index_offset4 =
+                (peer_offsets[chan_idx] + Offset) / sizeof(int4);
+            union {
+                BytesPack<16> data;
+                int4 val;
+            } ret;
+            ret.val = _ARK_SM_CHANS[chan_idx].read<int4>(index_offset4 + idx);
+            add_half8(tmp, ret.data);
+        }
+        store((ulonglong2 *)&dst[idx], tmp);
+    }
+}
+
+template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
+          unsigned int NPeers, unsigned int Rank, unsigned long long Offset,
+          unsigned long long Length>
+DEVICE void read_and_reduce(size_t src_offset_0, size_t src_offset_1,
+                            size_t src_offset_2, size_t src_offset_3,
+                            size_t src_offset_4, size_t src_offset_5,
+                            size_t src_offset_6, ark::fp16 *src, int uop_idx,
+                            int) {
+    // TODO: support length not multiple of 16
+    static_assert(Length % sizeof(int4) == 0, "Length must be multiple of 16");
+#if defined(ARK_TARGET_CUDA_ARCH)
+    return ring_read_and_reduce<Dims, Shape, UnitOutDims, NumWarps, NPeers,
+                                Rank, Offset, Length>(
+        src_offset_0, src_offset_1, src_offset_2, src_offset_3, src_offset_4,
+        src_offset_5, src_offset_6, src, uop_idx, 0);
+#else   // !defined(ARK_TARGET_CUDA_ARCH)
+    return parallel_read_and_reduce<Dims, Shape, UnitOutDims, NumWarps, NPeers,
+                                    Rank, Offset, Length>(
+        src_offset_0, src_offset_1, src_offset_2, src_offset_3, src_offset_4,
+        src_offset_5, src_offset_6, src, uop_idx, 0);
+#endif  // !defined(ARK_TARGET_CUDA_ARCH)
+}
+
 template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
           unsigned int NPeers, unsigned int Rank, unsigned long long Stride>
-DEVICE void gather_from_peers(size_t ori_offset, size_t target_offset_0,
-                              size_t target_offset_1, size_t target_offset_2,
-                              size_t target_offset_3, size_t target_offset_4,
-                              size_t target_offset_5, size_t target_offset_6,
-                              ark::fp16 *, int uop_idx, int) {
+DEVICE void ring_gather_from_peers(
+    size_t ori_offset, size_t target_offset_0, size_t target_offset_1,
+    size_t target_offset_2, size_t target_offset_3, size_t target_offset_4,
+    size_t target_offset_5, size_t target_offset_6, ark::fp16 *, int uop_idx,
+    int) {
     using UnitOp = UnitOp<Dims, Shape, UnitOutDims, NumWarps, 0>;
     constexpr size_t shape_width = Shape::W * sizeof(ark::fp16);
     constexpr size_t output_width = UnitOutDims::W * sizeof(ark::fp16);
@@ -207,6 +269,84 @@ DEVICE void gather_from_peers(size_t ori_offset, size_t target_offset_0,
                                         tid, UnitOp::NumThreads);
         }
     }
+}
+
+template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
+          unsigned int NPeers, unsigned int Rank, unsigned long long Stride>
+DEVICE void parallel_gather_from_peers(
+    size_t ori_offset, size_t target_offset_0, size_t target_offset_1,
+    size_t target_offset_2, size_t target_offset_3, size_t target_offset_4,
+    size_t target_offset_5, size_t target_offset_6, ark::fp16 *, int uop_idx,
+    int) {
+    using UnitOp = UnitOp<Dims, Shape, UnitOutDims, NumWarps, 0>;
+    constexpr size_t shape_width = Shape::W * sizeof(ark::fp16);
+    constexpr size_t output_width = UnitOutDims::W * sizeof(ark::fp16);
+    constexpr size_t stride = Dims::W * sizeof(ark::fp16);
+    const int tid = UnitOp::thread_id();
+    const int tile_hid = UnitOp::uop_idx_h(uop_idx);
+    const int tile_wid = UnitOp::uop_idx_w(uop_idx);
+    const size_t offset_in_width =
+        tile_wid * UnitOutDims::W * sizeof(ark::fp16);
+    size_t bytes_per_width = UnitOutDims::W * sizeof(ark::fp16);
+    if (offset_in_width + output_width > shape_width) {
+        bytes_per_width = shape_width - offset_in_width;
+    }
+    size_t peer_offsets[] = {target_offset_0, target_offset_1, target_offset_2,
+                             target_offset_3, target_offset_4, target_offset_5,
+                             target_offset_6};
+    const size_t unit_size = bytes_per_width >= (16 * UnitOp::NumThreads)
+                                 ? 16 * UnitOp::NumThreads
+                                 : bytes_per_width;
+    for (int i = tile_hid * UnitOutDims::H;
+         i < tile_hid * UnitOutDims::H + UnitOutDims::H; ++i) {
+        int base = 0;
+        for (; base < bytes_per_width; base += unit_size) {
+#pragma unroll
+            for (int j = 0; j < NPeers; ++j) {
+                int chan_idx = (Rank + j) % NPeers;
+                int remote_rank = chan_idx < Rank ? chan_idx : chan_idx + 1;
+                size_t offset = shape_width * remote_rank + i * stride +
+                                offset_in_width + base;
+                _ARK_SM_CHANS[chan_idx].get(peer_offsets[chan_idx] + offset,
+                                            ori_offset + offset, unit_size, tid,
+                                            UnitOp::NumThreads);
+            }
+        }
+        if (base < bytes_per_width) {
+#pragma unroll
+            for (int j = 0; j < NPeers; ++j) {
+                int chan_idx = (Rank + j) % NPeers;
+                int remote_rank = chan_idx < Rank ? chan_idx : chan_idx + 1;
+                size_t offset = shape_width * remote_rank + i * stride +
+                                offset_in_width + base;
+                _ARK_SM_CHANS[chan_idx].get(
+                    peer_offsets[chan_idx] + offset, ori_offset + offset,
+                    bytes_per_width - base, tid, UnitOp::NumThreads);
+            }
+        }
+    }
+}
+
+template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
+          unsigned int NPeers, unsigned int Rank, unsigned long long Stride>
+DEVICE void gather_from_peers(size_t ori_offset, size_t target_offset_0,
+                              size_t target_offset_1, size_t target_offset_2,
+                              size_t target_offset_3, size_t target_offset_4,
+                              size_t target_offset_5, size_t target_offset_6,
+                              ark::fp16 *, int uop_idx, int) {
+#if defined(ARK_TARGET_CUDA_ARCH)
+    return ring_gather_from_peers<Dims, Shape, UnitOutDims, NumWarps, NPeers,
+                                  Rank, Stride>(
+        ori_offset, target_offset_0, target_offset_1, target_offset_2,
+        target_offset_3, target_offset_4, target_offset_5, target_offset_6,
+        nullptr, uop_idx, 0);
+#else   // !defined(ARK_TARGET_CUDA_ARCH)
+    return parallel_gather_from_peers<Dims, Shape, UnitOutDims, NumWarps,
+                                      NPeers, Rank, Stride>(
+        ori_offset, target_offset_0, target_offset_1, target_offset_2,
+        target_offset_3, target_offset_4, target_offset_5, target_offset_6,
+        nullptr, uop_idx, 0);
+#endif  // !defined(ARK_TARGET_CUDA_ARCH)
 }
 
 template <typename Dims, typename Shape, typename UnitOutDims, int NumWarps,
