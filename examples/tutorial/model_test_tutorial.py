fix gradient update for ark params

examples/tutorial/model_test_tutorial.py
@@ -4,30 +4,45 @@
 import ark
 import torch
 import torch.optim as optim
+from ark import Module
 
-from ark.module import ARKFunction, ARKWrapper
+from ark.module import ARKComponent
 from ark import Tensor, Parameter
 
+# Let's first define a Linear layer in ARK
+class ARKLinear(ark.Module):
+    def __init__(self, weight):
+        super().__init__()
+        self.weight = weight
+        self.saved_inputs = None
 
-# Define a custom ARK function for a linear layer
-class ARKLinear(ARKFunction):
-    def forward(self, ark_input, ark_weight):
-        return ark.matmul(ark_input, ark_weight, transpose_other=True)
-
-    def backward(self, ark_grad_output, saved_context):
-        inp, weight = saved_context['input'], saved_context['weight']
-        grad_input = grad_weight = None
-        grad_input = ark.matmul(
-                ark_grad_output, weight, transpose_other=False
-            ) 
-        grad_weight = ark.matmul(inp, ark_grad_output, transpose_input=True)
-        return grad_input, grad_weight
+    def forward(self, inputs):
+        self.saved_inputs = inputs
+        return ark.matmul(inputs, self.weight, transpose_other=True)
     
-    def save_context(self, ctx_dict, idx, ark_input, ark_weight):
-        ctx_dict[idx] = {
-            'input': ark_input,
-            'weight': ark_weight
-        }
+    def backward(self, grad_output):
+        grad_weight = ark.matmul(grad_output, self.saved_inputs, transpose_input=True)
+        grad_input = ark.matmul(grad_output, self.weight, transpose_other=False)
+        # Update the gradient of the weight
+        self.weight.update_gradient(grad_weight)
+        return grad_input, grad_weight
+
+# Let's use our previous module to define a double linear layer
+class MyARKModule(ark.Module):
+    def __init__(self, weight1, weight2):
+        super().__init__()
+        self.linear1 = ARKLinear(weight1)
+        self.linear2 = ARKLinear(weight2)
+    
+    def forward(self, x):
+        x = self.linear1.forward(x)
+        x = self.linear2.forward(x)
+        return x
+    
+    def backward(self, grad_output):
+        grad_x, grad_weight2 = self.linear2.backward(grad_output)
+        grad_x, grad_weight1 = self.linear1.backward(grad_x)
+        return grad_x, grad_weight1, grad_weight2
 
 # Define a PyTorch model
 class SimpleModel(torch.nn.Module):
@@ -45,20 +60,35 @@ class SimpleModel(torch.nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+# Function to compare the gradients of two models of the same architecture and parameter order.
+def compare_grad(ark_model, torch_model, atol=1e-4, rtol=1e-2):
+    ark_params = list(ark_model.named_parameters()) 
+    torch_params = list(torch_model.named_parameters())
+    for ((ark_name, ark_param), (torch_name, torch_param)) in zip(ark_params, torch_params):
+        if (ark_param.grad is None) ^ (torch_param.grad is None):
+            print("Exactly one of the gradients is None")
+        else:
+            grads_equal = torch.allclose(ark_param.grad, torch_param.grad, atol=atol, rtol=rtol)
+            if not grads_equal:
+                print(f"Gradient for {ark_name} when compared to {torch_name} is different:")
+                print(f"ARK gradient: {ark_param.grad}")
+                print(f"Torch gradient: {torch_param.grad}")
+
 
 # For our ARK model we will replace the first two layers with ARK layers
 def replace_layers_with_ark(model):
-    weight_0 = torch.nn.Parameter(model.layers[0].weight.clone().detach().to("cuda:0").requires_grad_(True))
-    weight_1 = torch.nn.Parameter(model.layers[1].weight.clone().detach().to("cuda:0").requires_grad_(True))
-
-    model.layers[0] = ARKWrapper([ARKLinear(), ARKLinear()], [[weight_0], [weight_1]])    
+    weight_0 = torch.nn.Parameter(model.layers[0].weight.to("cuda:0").requires_grad_(True))
+    weight_1 = torch.nn.Parameter(model.layers[1].weight.to("cuda:0").requires_grad_(True))
+    ark_module = MyARKModule(weight_0, weight_1)
+    ark_wrapper = ARKComponent(ark_module)
+    model.layers[0] = ark_wrapper
     del model.layers[1]
-    model.register_parameter('weight_0', weight_0)
+
+    # Since we replaced the PyTorch layer with an ARK layer, we need to register the PyTorch parameters 
+    # our ARK module utilizes with the original PyTorch model so ARK can leverage PyTorch's optimizers.
+    model.register_parameter('weight_0', weight_0) 
     model.register_parameter('weight_1', weight_1)
     return model
-
-
-
 
 # Instantiate our models
 pytorch_model = SimpleModel()
@@ -66,30 +96,9 @@ ark_model = SimpleModel()
 
 
 
-            
-print("TORCH MODEL PARAMS:")
-for name, param in pytorch_model.named_parameters():
-    if param.requires_grad:
-        print(name, param, hex(param.data_ptr()), "REQUIRES GRAD")
-    else:
-        print(name, param, hex(param.data_ptr()), "NOGRAD")
-print('\n')
-
 # Ensure both models have the same weights
 ark_model.load_state_dict(pytorch_model.state_dict())
 ark_model = replace_layers_with_ark(ark_model)
-
-print("ARK MODEL PARAMS:")
-for name, param in ark_model.named_parameters():
-    if param.requires_grad:
-        print(name, param, hex(param.data_ptr()), "REQUIRES GRAD")
-    else:
-        print(name, param, hex(param.data_ptr()), "NO GRAD")
-print('\n')
-
-print("PyTorch model:\n", pytorch_model)
-print("\nARK model:\n", ark_model)
-
 
 # Move both models to GPU
 pytorch_model.to("cuda:0")
@@ -104,10 +113,10 @@ input_ark = input_torch.clone().detach().requires_grad_(True)
 target = torch.randn(128, 256).to("cuda:0")
 
 loss_fn = torch.nn.MSELoss()
-
 optim_torch = optim.SGD(pytorch_model.parameters(), lr=0.01)
 optim_ark = optim.SGD(ark_model.parameters(), lr=0.01)
-num_iters = 3
+
+num_iters = 5
 for iter in range(num_iters):
     print(f"Iteration {iter+1}/{num_iters}")
 
@@ -117,40 +126,22 @@ for iter in range(num_iters):
     pytorch_output = pytorch_model(input_torch)
     ark_output = ark_model(input_ark)
 
-    # The outputs for both models should be roughly the same
-    #assert torch.allclose(pytorch_output, ark_output, atol=1e-4, rtol=1e-2)
+    assert torch.allclose(pytorch_output, ark_output, atol=1e-4, rtol=1e-2)
 
     # Compute losses
     torch_loss = loss_fn(pytorch_output, target)
     ark_loss = loss_fn(ark_output, target)
 
     # Compare the results of both models
-    print(f"PyTorch loss: {torch_loss.item()}")
-    print(f"ARK loss: {ark_loss.item()}")
+    print(f"\nPyTorch loss: {torch_loss.item()}")
+    print(f"\nARK loss: {ark_loss.item()}\n")
 
     # Perform a backward pass
     torch_loss.backward()
     ark_loss.backward()
-    print("ARK MODEL GRADIENT CHECK:")
-    for name, param in ark_model.named_parameters():
-        if param.requires_grad:
-            if param.grad is not None:
-                print(name, param.size(), hex(param.data_ptr()), "HAS STORED GRADIENTS")
-            else:
-                print(name, param.size(), hex(param.data_ptr()), "REQUIRES GRAD BUT NO GRADIENT STORED")
-        else:
-            print(name, param.size(), hex(param.data_ptr()), "NO GRAD")
-    print('\n')
-    print('\n')
-    print("PYTORCH MODEL GRADIENT CHECK")
-    for name, param in pytorch_model.named_parameters():
-        if param.requires_grad:
-            if param.grad is not None:
-                print(name, param.size(), hex(param.data_ptr()), "HAS STORED GRADIENTS")
-            else:
-                print(name, param.size(), hex(param.data_ptr()), "REQUIRES GRAD BUT NO GRADIENT STORED")
-        else:
-            print(name, param.size(), hex(param.data_ptr()), "NO GRAD")
 
     optim_torch.step()
     optim_ark.step()
+
+    # Ensure gradients of both models are updated accordingly
+    compare_grad(ark_model, pytorch_model)

python/ark/module.py
@@ -37,11 +37,15 @@ class Module:
         """
         When setting an attribute, if the attribute is a Module, add it to
         the sub_modules. If the attribute is a Tensor and this Tensor is a
-        parameter, add it to the parameters.
+        parameter, add it to the parameters. If the attribute is a
+        torch.nn.Parameter, convert it to an ARK Parameter before adding.
         """
         if isinstance(__value, Module):
             self.register_module(__name, __value)
         elif isinstance(__value, Parameter):
+            self.register_parameter(__name, __value)
+        elif not _no_torch and isinstance(__value, torch.nn.Parameter):
+            __value = Parameter(__value)
             self.register_parameter(__name, __value)
         super().__setattr__(__name, __value)
 
@@ -196,86 +200,52 @@ class RuntimeModule(Module):
             return _recursive_ark_to_torch(self.forward_output)
 
 
-class ARKFunction:
-    def forward(self, input, *args): ...
-
-    def backward(self, grad_output, saved_context): ...
-
-    def save_context(self, ctx_dict, idx, *arg): ...
-
-
-class SharedARKFunction(Function):
+class ModuleFn(Function):
     """
     Base class for ARK functions.
     """
 
     @staticmethod
-    def forward(ctx, input, functions, args):
-        ctx.func = functions  # to store the ARK specific layers
-        ctx.args = args  # to store the parameters of the ARK specific layers
-        ctx.grad_weights = [None] * len(
-            args
-        )  # inits array holding potential gradient updates for the parameters of the ARK specific layers
-        ctx.ctx_dict = (
-            {}
-        )  # maps layers to their respective context dictionaries
-        output = Tensor.from_torch(input)
-        for i, (func, ag) in enumerate(zip(functions, args)):
-            ark_args = []
-            for j in ag:
-                print(
-                    "PROCESSING ARK PARAM: ",
-                    hex(j.data_ptr()),
-                    "for LAYER: ",
-                    i,
-                )
-                ark_param_view = Tensor.from_torch(j)
-                print("ARK PARAM VIEW: ", ark_param_view)
-                ark_args.append(ark_param_view)
-            output = func.forward(output, *ark_args)
-            func.save_context(ctx.ctx_dict, i, output, *ag)
+    def forward(ctx, input, ark_module):
+        """
+        Forward pass of the ARK module. Returns a PyTorch tensor that is the result
+        of the ARK module operation.
+        """
+        ark.init()
+        ctx.ark_module = ark_module
+        input_ark = Tensor.from_torch(input)
+        output = ark_module.forward(input_ark)
         with ark.Runtime.get_runtime() as rt:
             rt.launch(plan=ark.DefaultPlanner().plan())
             rt.run()
-            res = output.get_torch_view().clone()
-        print("CONTEXT DICT: ", ctx.ctx_dict)
-        return res
+            output = output.get_torch_view().clone()
+        return output
 
     @staticmethod
     def backward(ctx, *grad_outputs):
-        grad_input = Tensor.from_torch(grad_outputs[0])
-        grad_weights = [None] * len(ctx.func)
-        for i, func in enumerate(reversed(ctx.func)):
-            idx = len(ctx.func) - i - 1
-            mapped_dict = {
-                k: Tensor.from_torch(v) if isinstance(v, torch.Tensor) else v
-                for k, v in ctx.ctx_dict[idx].items()
-            }
-            grad_input, grad_weight = func.backward(grad_input, mapped_dict)
-            grad_weights[idx] = grad_weight
+        """
+        Converts the gradient outputs to ARK format, computes the gradients for the input 
+        and parameters using the ARK module backwards pass, and updates the gradients of the corresponding 
+        PyTorch parameters.
+        """
+        ark_grad_outputs = [Tensor.from_torch(grad) for grad in grad_outputs]
+        grad_input, *grad_weights = ctx.ark_module.backward(*ark_grad_outputs)
+        params_dict = ctx.ark_module.params_dict()
         with ark.Runtime.get_runtime() as rt:
             rt.launch(plan=ark.DefaultPlanner().plan())
             rt.run()
-            res = grad_input.get_torch_view().clone()
-            for i in range(len(grad_weights)):
-                if grad_weights[i] is not None:
-                    ctx.grad_weights[i] = grad_weights[i].get_torch_view()
-            print(
-                "GRAD WEIGHTS FOR ARK SPECIFIC LAYERS: ",
-                hex(ctx.grad_weights[0].data_ptr()),
-                hex(ctx.grad_weights[1].data_ptr()),
-            )
-        # Reverse to maintain order from forward pass
-        ctx.grad_weights.reverse()
-        # Return the accumulated grad_input
-        return (res, None, None)
+            output = grad_input.get_torch_view().clone()
+            for _, param in params_dict.items():
+                if param.staged_tensor is not None:
+                    pytorch_grad = param.staged_tensor.get_torch_view().clone()
+                    param.torch_param.grad = pytorch_grad
+        return (output, None)
 
 
-class ARKWrapper(TorchModule):
-    def __init__(self, functions, args):
+class ARKComponent(TorchModule):
+    def __init__(self, ark_module):
         super().__init__()
-        self.functions = functions
-        self.args = args
+        self.ark_module = ark_module
 
     def forward(self, input):
-        return SharedARKFunction.apply(input, self.functions, self.args)
+        return ModuleFn.apply(input, self.ark_module)

python/ark/tensor.py
@@ -121,16 +121,22 @@ class Tensor:
                 self.shape(), dtype=torch_type, device=torch.device(dev_name)
             )
         elif list(tensor.shape) != self.shape():
-            raise ValueError(f"torch tensor shape {list(tensor.shape)} "
-                             f"does not match the tensor {self.shape()}")
+            raise ValueError(
+                f"torch tensor shape {list(tensor.shape)} "
+                f"does not match the tensor {self.shape()}"
+            )
         elif tensor.dtype != torch_type:
-            raise ValueError(f"torch tensor dtype {tensor.dtype} "
-                             f"does not match the tensor {torch_type}")
+            raise ValueError(
+                f"torch tensor dtype {tensor.dtype} "
+                f"does not match the tensor {torch_type}"
+            )
         elif not tensor.is_contiguous():
             raise ValueError("torch tensor is not contiguous in memory")
         elif tensor.numel() != self.nelems():
-            raise ValueError(f"torch tensor size {tensor.numel()} "
-                             f"does not match the tensor {self.nelems()}")
+            raise ValueError(
+                f"torch tensor size {tensor.numel()} "
+                f"does not match the tensor {self.nelems()}"
+            )
         tensor_bytes = self.nelems() * self.dtype().element_size()
         rt.executor.tensor_read(
             self._tensor, tensor.data_ptr(), tensor_bytes, stream, True
@@ -235,16 +241,38 @@ class Parameter(Tensor):
     A tensor as a parameter.
     """
 
-    def __init__(self, _tensor: _Tensor, runtime_id: int = -1):
+    def __init__(
+        self, tensor: Union[_Tensor, torch.nn.Parameter], runtime_id: int = -1
+    ):
         """
         Initializes a new instance of the Parameter class.
         """
-        super().__init__(_tensor)
+        if _no_torch and isinstance(tensor, torch.nn.Parameter):
+            raise ValueError("torch is not available")
+        if isinstance(tensor, torch.nn.Parameter):
+            ark_tensor = Tensor.from_torch(tensor)
+            tensor_to_use = ark_tensor._tensor
+            self.torch_param = tensor
+            self.staged_tensor = None
+        elif isinstance(tensor, _Tensor):
+            tensor_to_use = tensor
+            self.torch_param = None
+        else:
+            raise TypeError(
+                "tensor must be an ARK tensor or a torch.nn.Parameter"
+            )
+
+        super().__init__(tensor_to_use, runtime_id=runtime_id)
         self.runtime_id = runtime_id
 
-    @staticmethod
-    def from_tensor(tensor: Tensor) -> "Parameter":
+    def update_gradient(self, ark_tensor: Tensor):
         """
-        Creates a Parameter from a Tensor.
+        Stages an ARK tensor to be used later for updating the gradient of its associated PyTorch parameter.
         """
-        return Parameter(tensor._tensor, tensor.runtime_id)
+        if _no_torch:
+            raise ImportError("torch is not available")
+        if self.torch_param is None:
+            raise ValueError("Parameter is not a torch.nn.Parameter")
+        if not self.torch_param.requires_grad:
+            raise ValueError("Parameter does not require gradient")
+        self.staged_tensor = ark_tensor