f758794c5e3f78ec92d721a53e3f9d5810e8768f
noli <t-ngerawork@microsoft.com>
2024-07-25 15:55:29 +0000

    multiple executor

--- python/ark/runtime.py
+++ python/ark/runtime.py
@@ -75,15 +75,29 @@ class Runtime:
 
     def __init__(self, runtime_id: int = -1):
         self.runtime_id = runtime_id
-        self.executor: Executor = None
-        self.state: Runtime.State = Runtime.State.Init
+        self.executor_map: Dict[int, Executor] = {}
+        self.executor_states: Dict[int, Runtime.State] = {}
         _RuntimeState.runtime[runtime_id] = self
 
-    def get_state(self) -> "Runtime.State":
+    @property
+    def state(self) -> "Runtime.State":
         """
-        Get the runtime state.
+        Returns the runtime state of the default executor.
         """
-        return self.state
+        return self.executor_states.get(-1, Runtime.State.Init)
+
+    @property
+    def executor(self) -> "Executor":
+        """
+        Returns the default executor.
+        """
+        return self.executor_map.get(-1, None)
+
+    def get_state(self, executor_id: int = -1) -> "Runtime.State":
+        """
+        Returns the runtime state of the executor with the given ID.
+        """
+        return self.executor_states.get(executor_id, Runtime.State.Init)
 
     @staticmethod
     def exists(runtime_id: int) -> bool:
@@ -122,20 +136,20 @@ class Runtime:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.reset()
 
-    def launched(self) -> bool:
+    def launched(self, executor_id: int = -1) -> bool:
         """
         Check if the runtime is launched.
         """
-        return (
-            self.state == Runtime.State.LaunchedNotRunning
-            or self.state == Runtime.State.Running
-        )
+        return self.get_state(executor_id=executor_id) in {
+            Runtime.State.LaunchedNotRunning,
+            Runtime.State.Running
+        }
 
-    def running(self) -> bool:
+    def running(self, executor_id: int = -1) -> bool:
         """
         Check if the runtime is running.
         """
-        return self.state == Runtime.State.Running
+        return self.get_state(executor_id) == Runtime.State.Running 
 
     def launch(
         self,
@@ -143,88 +157,128 @@ class Runtime:
         device_id: int = 0,
         stream: int = 0,
         loop_mode: bool = True,
+        executor_id: int = -1
     ):
         """
         Create an executor and schedule the ARK model. The scheduler will generate
         the CUDA kernels. The GPU context and the connection between GPUs will be
         initialized. The executor will compile the cuda kernels and launch the ARK runtime.
         """
-        if self.launched():
+        if self.launched(executor_id=executor_id):
             logging.warning(
-                f"Runtime {self.runtime_id} is already launched, skip launching"
+                f"{('Default executor' if executor_id == -1 else f'Executor {executor_id}')}"
+                f" of runtime {self.runtime_id} is already launched, skip launching"
             )
             return
         if plan is None:
             plan = DefaultPlanner(device_id).plan()
         # If the RuntimeState is init, we need to create a new executor and
         # compile the kernels
-        if self.state == Runtime.State.Init:
-            if self.executor is not None:
-                if not self.executor.destroyed():
-                    logging.warning(
-                        f"Runtime {self.runtime_id}, has already been launched. Destroying the old executor"
+        if self.get_state(executor_id=executor_id) == Runtime.State.Init:
+            if executor_id in self.executor_map:
+                if not self.executor_map[executor_id].destroyed():
+                    executor_desc = (
+                        "Default Executor"
+                        if executor_id == -1
+                        else f"Executor {executor_id}"
                     )
-                    self.executor.destroy()
-            self.executor = Executor(
+                    logging.warning(
+                        f'''{executor_desc} associated with runtime {self.runtime_id}, 
+                            has already been launched. Destroying the old executor'''
+                    )
+                    self.executor_map[executor_id].destroy()
+            executor = Executor(
                 device_id,
                 stream,
                 "ArkRuntime",
                 plan,
                 loop_mode,
             )
-            self.executor.compile()
-        self.executor.launch()
-        self.state = Runtime.State.LaunchedNotRunning
+            self.executor_map[executor_id] = executor
+            executor.compile()
+        self.executor_map[executor_id].launch()
+        self.executor_states[executor_id] = Runtime.State.LaunchedNotRunning
 
-    def run(self, iter=1, non_blocking=False):
+    def run(self, iter=1, non_blocking=False, executor_id: int = -1):
         """
         Run the ARK program for iter iterations and wait for the kernel to finish.
         """
-        if self.state != Runtime.State.LaunchedNotRunning:
-            logging.error(f"ARK runtime {self.runtime_id} is not launched")
-            raise RuntimeError(f"ARK runtime {self.runtime_id} is not launched")
-        self.state = Runtime.State.Running
-        self.executor.run(iter)
+        if self.get_state(executor_id) != Runtime.State.LaunchedNotRunning:
+            executor_desc = (
+                        "Default Executor"
+                        if executor_id == -1
+                        else f"Executor {executor_id}"
+                    )
+            logging.error(f"{executor_desc} of ARK runtime {self.runtime_id} is not launched")
+            raise RuntimeError(f"{executor_desc} of ARK runtime {self.runtime_id} is not launched")
+        self.executor_states[executor_id] = Runtime.State.Running
+        self.executor_map[executor_id].run(iter)
         if not non_blocking:
-            self.wait()
+            self.wait(executor_id)
 
-    def wait(self):
+    def wait(self, executor_id: int = -1):
         """
         Wait for the kernel to finish.
         """
-        if self.state != Runtime.State.Running:
+        if self.get_state(executor_id) != Runtime.State.Running:
+            executor_desc = (
+                        "Default Executor"
+                        if executor_id == -1
+                        else f"Executor {executor_id}"
+                    )
             logging.warning(
-                f"ARK runtime {self.runtime_id} is not running, skip waiting"
+                f"{executor_desc} associated with runtime {self.runtime_id} is not running, skip waiting"
             )
             return
-        self.executor.wait()
-        self.state = Runtime.State.LaunchedNotRunning
+        self.executor_map[executor_id].wait()
+        self.executor_states[executor_id] = Runtime.State.LaunchedNotRunning
 
-    def stop(self) -> float:
+    def stop(self, executor_id = -1) -> float:
         """
         Stop the model and return the elapsed time in milliseconds.
         Once this is called, we need to call `launch()` again to run the model again.
         """
-        if not self.launched():
+        if not self.launched(executor_id):
+            executor_desc = (
+                        "Default Executor"
+                        if executor_id == -1
+                        else f"Executor {executor_id}"
+                    )
             logging.warning(
-                f"ARK runtime {self.runtime_id} is never launched, skip stopping"
+                f"{executor_desc} assocaited with runtime {self.runtime_id} is never launched, skip stopping"
             )
             return
-        elapsed = self.executor.stop()
-        self.state = Runtime.State.LaunchedNotRunning
+        elapsed = self.executor_map[executor_id].stop()
+        self.executor_states[executor_id] = Runtime.State.LaunchedNotRunning
         return elapsed
+    
+    def reset_executor(self, executor_id: int = -1, persist=False):
+        """
+        Reset a specific executor associated with the runtime.
+        """
+        if self.launched(executor_id):
+            self.stop(executor_id)
+        if persist:
+            return
+        if executor_id in self.executor_map:
+            if not self.executor_map[executor_id].destroyed():
+                self.executor_map[executor_id].destroy()
+            del self.executor_map[executor_id]
+            del self.executor_states[executor_id]
+        
 
     def reset(self, delete=False):
         """
         Reset the runtime. If delete is True, delete the runtime associated with the runtime_id.
         """
-        if self.launched():
-            self.stop()
-        if self.executor is not None:
-            if not self.executor.destroyed():
-                self.executor.destroy()
-            self.executor = None
-        self.state = Runtime.State.Init
+        for executor_id in list(self.executor_map.keys()):
+            if self.launched(executor_id):
+                self.stop(executor_id)
+            if (executor_id in self.executor_map and
+                not self.executor_map[executor_id].destroyed()):
+                self.executor_map[executor_id].destroy()
+        self.executor_map.clear()
+        self.executor_states.clear()
         if delete:
             del _RuntimeState.runtime[self.runtime_id]
 
--- python/unittest/test_runtime.py
+++ python/unittest/test_runtime.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 import ark
+import numpy as np
 
 
 empty_plan = ark.Plan(None)
@@ -116,3 +117,77 @@ def test_multiple_runtimes_init():
     runtimes = ark.Runtime.see_runtime_statuses()
     assert len(runtimes) == 0
     ark.Runtime.delete_all_runtimes()
+
+# Executor tests
+
+
+def test_adding_executors():
+    ark.init()
+    runtime = ark.Runtime.get_runtime()
+    num_executors = 3
+    for i in range(num_executors):
+        runtime.launch(plan=empty_plan, executor_id=i, device_id=i)
+    assert len(runtime.executor_map) == num_executors
+    assert len(runtime.executor_states) == num_executors
+    for i in range(num_executors):
+        assert i in runtime.executor_map
+        assert (
+            runtime.executor_states[i] == ark.Runtime.State.LaunchedNotRunning
+        )
+    ark.Runtime.delete_all_runtimes()
+
+
+def test_getting_executor_state():
+    ark.init()
+    runtime = ark.Runtime.get_runtime()
+    executor_ids = [1, 2, 3]
+    assert runtime.get_state(1) == ark.Runtime.State.Init
+    assert runtime.get_state(2) == ark.Runtime.State.Init
+    assert runtime.get_state(3) == ark.Runtime.State.Init
+    for id in executor_ids:
+        runtime.launch(plan=empty_plan, executor_id=id, device_id=id)
+        assert runtime.get_state(id) == ark.Runtime.State.LaunchedNotRunning
+    ark.Runtime.delete_all_runtimes()
+    assert runtime.get_state(1) == ark.Runtime.State.Init
+    assert runtime.get_state(2) == ark.Runtime.State.Init
+    assert runtime.get_state(3) == ark.Runtime.State.Init
+
+
+def test_distinct_exec_states():
+    ark.init()
+    rt1, rt2 = ark.Runtime.get_runtime(1), ark.Runtime.get_runtime(2)
+    executor_id = 1
+    rt1.launch(plan=empty_plan, executor_id=executor_id, device_id=1)
+    assert rt1.get_state(executor_id) == ark.Runtime.State.LaunchedNotRunning
+    assert rt2.get_state(executor_id) == ark.Runtime.State.Init
+    rt2.launch(plan=empty_plan, executor_id=executor_id, device_id=2)
+    assert rt2.get_state(executor_id) == ark.Runtime.State.LaunchedNotRunning
+    rt1.reset()
+    assert rt1.get_state(executor_id) == ark.Runtime.State.Init
+    assert rt2.get_state(executor_id) == ark.Runtime.State.LaunchedNotRunning
+    rt2.reset()
+    assert rt2.get_state(executor_id) == ark.Runtime.State.Init
+    ark.Runtime.delete_all_runtimes()
+
+
+def test_multi_executor():
+    ark.init()
+    M, N = 64, 64
+    input_tensor = ark.tensor([M, N], ark.fp16)
+    other_tensor = ark.tensor([M, N], ark.fp16)
+    output_tensor = ark.add(input_tensor, other_tensor)
+    runtime = ark.Runtime()
+    executor_ids = [0, 1]
+    for i in executor_ids:
+        runtime.launch(executor_id=i)
+        input_tensor_host = np.random.rand(M, N).astype(np.float16)
+        input_tensor.from_numpy(input_tensor_host, executor_id=i)
+        other_tensor_host = np.random.rand(M, N).astype(np.float16)
+        other_tensor.from_numpy(other_tensor_host, executor_id=i)
+        runtime.run(executor_id=i)
+        output_tensor_host = output_tensor.to_numpy(executor_id=i)
+        np.testing.assert_allclose(
+            output_tensor_host, input_tensor_host + other_tensor_host
+        )
+        runtime.stop(executor_id=i)
+    runtime.reset()
