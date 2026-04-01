856716180ff752f834a0664e059ee970684253a8
Changho Hwang <changhohwang@microsoft.com>
2026-04-01 11:30:26 +0000

    placeholder tests

--- python/unittest/test.py
+++ python/unittest/test.py
@@ -5,6 +5,7 @@ from test_data_type import *
 from test_error import *
 from test_model import *
 from test_ops import *
+from test_placeholder import *
 from test_planner import *
 from test_runtime import *
 from test_tensor import *
