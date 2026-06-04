e312d31092cc18d30db323e39a8cfd390f4c5a7b
Changho Hwang <changhohwang@microsoft.com>
2026-06-04 08:47:40 +0000

    Fix communication test task counts

--- ark/ops/ops_communication_test.cpp
+++ ark/ops/ops_communication_test.cpp
@@ -194,7 +194,7 @@ ark::unittest::State test_communication_send_recv_bidir_sm() {
             config["ChannelType"] = "Sm";
             config["Signal"] = true;
             config["Tile"] = {1, 256};
-            config["NumTasks"] = 2;
+            config["NumTasks"] = 4;
             config["NumWarps"] = 4;
             config["SramBytes"] = 0;
         } else if (op.at("Type") == "SendDone") {
