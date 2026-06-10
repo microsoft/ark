d5d8e6c2bfd96e340f65ac069a4d2c6595730dae
ark-dev <ark-dev@local>
2026-06-10 01:19:08 +0000

    ark-dev: Update PR #253 by merging base branch  into head branch  and resolving any conflicts; cause: BEHIND (behind_by=1).

--- ark/ops/ops_all_reduce_test.cpp
+++ ark/ops/ops_all_reduce_test.cpp
@@ -252,6 +252,7 @@ void test_all_reduce_inplace_internal(ark::DimType nelem) {
     for (int gpu_id = 0; gpu_id < NumGpus; ++gpu_id) {
         ark::unittest::spawn_process([gpu_id, nelem]() {
             UNITTEST_SKIP(ark::unittest::get_gpu_count() < NumGpus);
+            // Each GPU's data is equal to its GPU ID + 1.
             ark::Model m(gpu_id, NumGpus);
             ark::Tensor ones = m.tensor({nelem}, ark::FP16);
             ark::Tensor data = m.mul(ones, float(gpu_id + 1));
